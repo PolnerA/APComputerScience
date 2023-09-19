47408b19aaf43c78f47c599318d3bbff1129b231
polnera <adampolner@gmail.com>
2023-09-19 10:18:06 -0700

    Added Top comment

--- src/GS02_01.java
+++ src/GS02_01.java
@@ -1,3 +1,15 @@
+/*
+Name: Adam Polner
+Problem: Write a program to output the squares of the numbers 1 to a given number.
+         (Challenge yourself to do it without multiplying or using powers.)
+Pseudocode: Ask up to which number they want, use it as the number of loops, 1+3 each time add 2 to 3 and keep adding
+            the number
+Notes: 1+3 = 4+5 = 9+7 = 16+9 = 25+11 = 36+13 = 49 (number +3, +2 to the 3 every iteration)
+Maintenance log:
+Date:           Done:
+9/19/2023       Finished GS02-01
+*/
+
 import java.util.Scanner;
 
 public class GS02_01 {
@@ -7,7 +19,6 @@ public class GS02_01 {
         int max =sc.nextInt();
         int num=1;
         int add=3;
-        //1+3 = 4+5 = 9+7 = 16+9 = 25+11 = 36+13 = 49
         for(int i=1; i<=max;i++)
         {
             System.out.print(num+"\t");
