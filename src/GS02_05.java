c9302862a26099ae0d04b205c2fffd5cafd2323c
polnera <adampolner@gmail.com>
2023-09-22 08:50:35 -0700

    Started GS02-05

---
+++ src/GS02_05.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class GS02_05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("First Side:");
+        double side1= sc.nextDouble();
+        System.out.println("Second Side:");
+        double side2= sc.nextDouble();
+        System.out.println("Third Side:");
+        double side3= sc.nextDouble();
+    }
+}
