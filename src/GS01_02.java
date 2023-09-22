dd23766ce0588df27ba2eab66daef0178ef7e91c
polnera <adampolner@gmail.com>
2023-09-22 09:03:13 -0700

    Finished GS01-02

--- src/GS01_02.java
+++ src/GS01_02.java
@@ -1,2 +1,20 @@
+/*
+Name: Adam Polner
+Problem: BJP Chapter 1 exercise 7
+Pseudocode:print top line then repeat the next two five times
+Notes:
+Maintenance log:
+Date:           Done:
+9/22/2023       Finished GS01-02
+*/
 public class GS01_02 {
+    public static void main(String[] args) {
+        System.out.print("/ / / / / / / / / / / ");
+        Victory();
+    }
+    public static void Victory() {
+        for(int i=0;i<=5;i++){
+        System.out.print("\n|| Victory is mine! ||\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        }
+    }
 }
