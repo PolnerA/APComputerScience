be812a59aa3abf065390409fef0b2c4c1a092e01
polnera <adampolner@gmail.com>
2023-09-25 09:38:20 -0700

    Started working on GS02-03

--- src/GS02_03.java
+++ src/GS02_03.java
@@ -3,17 +3,23 @@ public class GS02_03 {
         DrawStair(1);
     }
     static void DrawStair(int steps) {
-        for (int i=0;i<=steps*5;i++) {
-            if(i==(steps*5)-3) {
-                System.out.print("o");
-            }
-            else{
-                System.out.print(" ");
-            }
+        //draw set amount of spaces +2 then an o then 2 spaces followed by 6 * then a * at the end depending on height
+        //after same set amount of spaces +1 space then a /|\ (\\) then a space and an *, amount of spaces vary, with an * to end it off
+        //same set amount of spaces +1 space then a / with a space then \ (\\) then a space followed by * amount of spaces vary *
+        DrawTopStair(steps);
+    }
+    static void DrawTopStair(int steps) {
+        for (int i = 0; i <= 2 + ((steps-1) * 5); i++) {
+        System.out.print(" ");
         }
-        for(int i=0;i<=6;i++) {
-            System.out.print("*");
+        System.out.print("o");
+        System.out.print("  ");
+        System.out.print("*******\n");
+        for (int i = 0; i <= 1 + ((steps-1) * 5); i++) {
+            System.out.print(" ");
         }
+        System.out.print("/|\\ *");
+
     }
 }
 //
