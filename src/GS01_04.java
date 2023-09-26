331fe3911075ee32f28a08871348ff672044fbb6
polnera <adampolner@gmail.com>
2023-09-26 10:52:18 -0700

    Started GS01-04

--- src/GS01_04.java
+++ src/GS01_04.java
@@ -1,5 +1,29 @@
 public class GS01_04 {
     public static void main(String[] args) {
-
+        System.out.println("There was an old lady who swallowed a fly.");
+        IDKY();
+        System.out.println("There was an old lady who swallowed a spider,\nThat wriggled and iggled and jiggled inside her.");
+        Spider();
+        System.out.println("There was an old lady who swallowed a bird,\nHow absurd to swallow a bird");
+        Bird();
     }
+    public static void IDKY(){
+        System.out.println("I don't know why she swallowed that fly,\nPerhaps She'll die.");
+    }
+    public static void Spider(){
+        System.out.println("She swallowed the spider to catch the fly,");
+        IDKY();
+    }
+    public static void Bird() {
+        System.out.println("She swallowed the bird to catch the spider,");
+        Spider();
+    }public static void Cat() {
+        System.out.println("She swallowed the cat to catch the bird,");
+        Bird();
+    }
+    public static void Dog(){
+        System.out.println("She swallowed teh dog to catch the cat,");
+        Cat();
+    }
+
 }
