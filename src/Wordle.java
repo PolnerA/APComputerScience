0d7bb1e7bdb6ec75428e2efbb0cff4cacd69bfa1
polnera <adampolner@gmail.com>
2023-10-05 08:56:21 -0700

    Continued programming Wordle

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,14 +1,16 @@
+import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
+
 public class Wordle {
     public static void main(String[] args) {
         try{
             Random rng = new Random();
             Scanner sc = new Scanner(System.in);
-            File myObj = new File("/Users/polnera/IdeaProjects/APComputerScience/src/FiveLetterWords.txt");
-            Scanner myReader = new Scanner(myObj);
+            File WordleAns = new File("/Users/polnera/IdeaProjects/APComputerScience/src/WordleAns.txt");
+            File FiveLetterWords = new File("/Users/polnera/IdeaProjects/APComputerScience/src/FiveLetterWords.txt");
+            Scanner myReader = new Scanner(WordleAns);
+
             String word = "error";
             for(int i=0;i<= rng.nextInt(15919);i++) {//15919
                 word = myReader.nextLine();
@@ -20,21 +22,21 @@ public class Wordle {
             chararray[2]=word.charAt(2);
             chararray[3]=word.charAt(3);
             chararray[4]=word.charAt(4);
+            int[] statuschar = new int[5];
             for (int j=1;j<=6;j++) {
                 System.out.print("Five letter word: ");
                 String fiveletters = sc.nextLine();
                 boolean realword = false;
-                Scanner search = new Scanner(myObj);
+                Scanner search = new Scanner(FiveLetterWords);
                 while (search.hasNextLine()) {
                     String line = search.nextLine();
                     if (line.equals(fiveletters)) {
                         realword = true;
-                        System.out.println("It matches");
                         break;
                     }
                 }
                 search.close();
-                Scanner loopsearch = new Scanner(myObj);
+                Scanner loopsearch = new Scanner(FiveLetterWords);
                 while (fiveletters.length() != 5 || !realword) {
                     System.out.print("Five letter word: ");
                     fiveletters = sc.nextLine();
@@ -42,12 +44,11 @@ public class Wordle {
                         String line = loopsearch.nextLine();
                         if (line.equals(fiveletters)) {
                             realword = true;
-                            System.out.println("It matches");
                             break;
                         }
+                        loopsearch.reset();
                     }
                 }
-                int[] statuschar = new int[5];
                 //automatically gray (Gray 0 | Yellow 1 | Green 2)
                 for (int i = 0; i < 5; i++) {
                     statuschar[i] = 0;
@@ -60,13 +61,25 @@ public class Wordle {
                     }
                 }
                 for (int i = 0; i < 5; i++) {
-                    System.out.println(statuschar[i]);
+                    if(statuschar[i]==0){
+                        System.out.print("Grey.");
+                    }
+                    else if(statuschar[i]==1){
+                        System.out.print("Yellow.");
+                    }
+                    else{
+                        System.out.print("Green.");
+                    }
                 }
+                System.out.println();
                 if(statuschar[0]==2&&statuschar[1]==2&&statuschar[2]==2&&statuschar[3]==2&&statuschar[4]==2){
-                    System.out.println("The word was "+word);
                     System.out.println("You took "+j+" guesses");
+                    break;
                 }
+
             }
+            System.out.println("The word was "+word);
+
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
