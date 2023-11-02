b624491c7bcfdd67fe7f5c597b583857a1dd5583
polnera <adampolner@gmail.com>
2023-11-02 08:56:35 -0700

    Started solver with files of Mastermind_#p#c.txt

--- src/MastermindSolver.java
+++ src/MastermindSolver.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+
 public class MastermindSolver {
     public static void main(String[] args) {
+        ArrayList<Integer> possibilities = new ArrayList<Integer>();
+        /*list would help to know what to remove if index is known as opposed to .txt which would have to
+          read every line until given number is found. populate index with .txt, make a new one for different pins
+          and colors with the following code:
+          int max = colors-1
+          for(int i=0;i<pins;i++){
+            max *= 10;
+            max += colors-1;
+          }
+          for(int i =0; i<=max;i++){
 
+          }
+        */
+        Solve();
+    }
+    public static void Solve(){
+        String guess="1122";
+        int guesses=1;//increments guesses anytime a guess is taken (new string guess)
+        String code=MasterMind.GenerateCode();
+        int[] blacknwhitepins = MasterMind.EvaluateGuess(guess,code);
+        int blackpins = blacknwhitepins[0];
+        int whitepins = blacknwhitepins[1];
+        while(blackpins!=4){
+
+        }
     }
 }
