6d2b1f42e3be3efceb658e7c832d217d79850d9d
Adam Polner <polnera@rsd407.org>
2023-11-05 15:57:55 -0800

    made the first guess a class constant

--- src/MastermindSolver.java
+++ src/MastermindSolver.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class MastermindSolver {
     private static final String testFilename = "mastermind_4p6c.txt";
+    private static final String firstguess = "0011";
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<String> zeroblackpins = new ArrayList<String>();
         ArrayList<String> oneblackpins = new ArrayList<String>();
@@ -39,7 +40,7 @@ public class MastermindSolver {
         */
     }
     public static void Solve(){
-        String guess="0011";
+        String guess=firstguess;
         int guesses=1;//increments guesses anytime a guess is taken (new string guess)
         String code=MasterMind.GenerateCode();
         int[] blacknwhitepins = MasterMind.EvaluateGuess(guess,code);
