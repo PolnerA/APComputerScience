8f4b3f489e88384e2344f06c01ddff425f3b414d
Adam Polner <polnera@rsd407.org>
2023-11-05 15:56:53 -0800

    Cont. solver

--- src/MastermindSolver.java
+++ src/MastermindSolver.java
@@ -1,24 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MastermindSolver {
-    public static void main(String[] args) {
-        ArrayList<Integer> possibilities = new ArrayList<Integer>();
+    private static final String testFilename = "mastermind_4p6c.txt";
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> zeroblackpins = new ArrayList<String>();
+        ArrayList<String> oneblackpins = new ArrayList<String>();
+        ArrayList<String> twoblackpins = new ArrayList<String>();
+        ArrayList<String> threeblackpins = new ArrayList<String>();
+        ArrayList<String> fourblackpins = new ArrayList<String>();
+        FileReader mastermindutputs = new FileReader(testFilename);
+        Scanner lineScanner = new Scanner(mastermindutputs);
+        while(lineScanner.hasNextLine()){
+            String line = lineScanner.nextLine();
+            char blackpins = line.charAt(10);//specific to 4 pin (pins*2+2)
+            if(blackpins-'0'==0){
+                zeroblackpins.add(line);
+            }
+            else if(blackpins-'0'==1){
+                oneblackpins.add(line);
+            }
+            else if(blackpins-'0'==2){
+                twoblackpins.add(line);
+            }
+            else if(blackpins-'0'==3){
+                threeblackpins.add(line);
+            }
+            else{
+                fourblackpins.add(line);
+            }
+        }
         /*list would help to know what to remove if index is known as opposed to .txt which would have to
-          read every line until given number is found. populate index with .txt, make a new one for different pins
-          and colors with the following code:
-          int max = colors-1
-          for(int i=0;i<pins;i++){
-            max *= 10;
-            max += colors-1;
-          }
-          String num="";
-          for(int i =0; i<=max;i++){
-
-          }
+          read every line until given number is found. populate index with .txt, make a new one for different
+          pins + colors
         */
     }
     public static void Solve(){
-        String guess="1122";
+        String guess="0011";
         int guesses=1;//increments guesses anytime a guess is taken (new string guess)
         String code=MasterMind.GenerateCode();
         int[] blacknwhitepins = MasterMind.EvaluateGuess(guess,code);
