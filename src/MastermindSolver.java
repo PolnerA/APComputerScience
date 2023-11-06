cc92de3ebde3117ebdaf09ff29ea5617227683da
polnera <adampolner@gmail.com>
2023-11-06 09:39:01 -0800

    split the data from the .txt

--- src/MastermindSolver.java
+++ src/MastermindSolver.java
@@ -8,31 +8,12 @@ public class MastermindSolver {
     private static final String testFilename = "mastermind_4p6c.txt";
     private static final String firstguess = "0011";
     public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<String> zeroblackpins = new ArrayList<String>();
-        ArrayList<String> oneblackpins = new ArrayList<String>();
-        ArrayList<String> twoblackpins = new ArrayList<String>();
-        ArrayList<String> threeblackpins = new ArrayList<String>();
-        ArrayList<String> fourblackpins = new ArrayList<String>();
+        ArrayList<String> Possibilities = new ArrayList<String>();
         FileReader mastermindutputs = new FileReader(testFilename);
         Scanner lineScanner = new Scanner(mastermindutputs);
         while(lineScanner.hasNextLine()){
             String line = lineScanner.nextLine();
-            char blackpins = line.charAt(10);//specific to 4 pin (pins*2+2)
-            if(blackpins-'0'==0){
-                zeroblackpins.add(line);
-            }
-            else if(blackpins-'0'==1){
-                oneblackpins.add(line);
-            }
-            else if(blackpins-'0'==2){
-                twoblackpins.add(line);
-            }
-            else if(blackpins-'0'==3){
-                threeblackpins.add(line);
-            }
-            else{
-                fourblackpins.add(line);
-            }
+            String[] data = line.split(",");
         }
         /*list would help to know what to remove if index is known as opposed to .txt which would have to
           read every line until given number is found. populate index with .txt, make a new one for different
