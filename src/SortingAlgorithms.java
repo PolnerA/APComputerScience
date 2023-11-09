fd6987ac09b0ff5394a8688f8d0f3ada9ff08173
polnera <adampolner@gmail.com>
2023-11-09 08:26:31 -0800

    Changed comments

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,6 +1,5 @@
 public class SortingAlgorithms {
-    public static void main(String[] args) {
-//selection, merge, bubble, quick
+    public static void main(String[] args) {//selection, merge, bubble, quick, double selection sort
     }
     public static void SelectionSort(int[] list){//O(N^2) performance
         for(int i=0;i< list.length-1;i++){
@@ -13,7 +12,7 @@ public class SortingAlgorithms {
             Swap(list,i,smallest);
         }
     }
-    public static void ModifiedSelectionSort(int[] list){//O(N^2) performance
+    public static void ModifiedSelectionSort(int[] list){//O(N^2) performance runs faster
         for(int i=0;i< list.length-1;i++){
             int largest=i;
             for(int j=i+1;j<list.length;j++){
