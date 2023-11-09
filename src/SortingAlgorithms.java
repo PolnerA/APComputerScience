c8fbbcb736ce2a23fe0dc9197ffd4211f398c96c
polnera <adampolner@gmail.com>
2023-11-09 08:12:37 -0800

    Started SortingAlgorithms project

---
+++ src/SortingAlgorithms.java
@@ -0,0 +1,32 @@
+public class SortingAlgorithms {
+    public static void main(String[] args) {
+//selection, merge, bubble, quick
+    }
+    public static void SelectionSort(int[] list){//O(N^2) performance
+        for(int i=0;i< list.length-1;i++){
+            int smallest=i;
+            for(int j=i+1;j<list.length;j++){
+                if(list[j]<list[smallest]){
+                    smallest=j;
+                }
+            }
+            Swap(list,i,smallest);
+        }
+    }
+    public static void ModifiedSelectionSort(int[] list){//O(N^2) performance
+        for(int i=0;i< list.length-1;i++){
+            int largest=i;
+            for(int j=i+1;j<list.length;j++){
+                if(list[largest]<list[j]){
+                    largest=j;
+                }
+            }
+            Swap(list,i,largest);
+        }
+    }
+    public static void Swap(int[] list,int i, int j){
+        int temp = list[i];
+        list[i]=list[j];
+        list[j]=temp;
+    }
+}
