cdec23ac12b6929ade6f30308f4a735ecbde7584
polnera <adampolner@gmail.com>
2023-12-04 09:30:38 -0800

    Changed the keyboard input to mouse input

--- src/Snake.java
+++ src/Snake.java
@@ -1,103 +1,70 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
+
 public class Snake {
+    static int snakelength = 1;
+    static int snakex = 0;
+    static int snakey = 0;
+
+    enum directions {up, left, down, right};
+    static directions snakedirections = directions.right;
     public static void main(String[] args) throws InterruptedException {
-        Thread maingame = new Thread(new Game1());
-        maingame.run();
-        Thread input = new Thread(new Keyboard());
-        input.run();
-    }
-    public static class Game1 implements Runnable{
-        int snakelength = 1;
-        int snakex = 0;
-        int snakey = 0;
-
-        public enum directions {up, left, down, right};
-        public static directions snakedirections = directions.right;
-        public Game1(){
-
-        }
-        @Override
-        public void run() {
-            try {
-                while (true) {
-                    switch (snakedirections) {
-                        case up:
-                            snakey--;
-                            break;
-                        case left:
-                            snakex--;
-                            break;
-                        case down:
-                            snakey++;
-                            break;
-                        case right:
-                            snakex++;
-                            break;
-                    }
-                    DrawSnake();
-                    System.out.print("\033[H\033[2J");
-                }
-            } catch (Exception e) {
-                System.out.print("Exception is caught.");
+        /*int mousex =MouseInfo.getPointerInfo().getLocation().x;
+        int mousey =MouseInfo.getPointerInfo().getLocation().y;
+        if(0<(mousex + 2048/2)-(2048/2)){
+            snakedirections=directions.right;
+        }//2048 X 1152
+        else if(20<mousey){
+            snakedirections=directions.down;
+        }*/
+        Random rng = new Random();
+        while (true) {
+            int randomdir = rng.nextInt(4);
+            if(randomdir==0){
+                snakedirections=directions.up;
+            } else if (randomdir==1) {
+                snakedirections=directions.left;
+            }else if (randomdir==2) {
+                snakedirections=directions.down;
+            }else if (randomdir==3) {
+                snakedirections=directions.right;
             }
-        }
-        public void DrawSpace() {
-            for (int i = 0; i < snakey; i++) {
-                System.out.println();
+            switch (snakedirections) {
+                case up:
+                    snakey--;
+                    break;
+                case left:
+                    snakex--;
+                    break;
+                case down:
+                    snakey++;
+                    break;
+                case right:
+                    snakex++;
+                    break;
             }
-            for (int i = 0; i < snakex; i++) {
-                System.out.print(" ");
-            }
-        }
-        public void DrawSnake() throws InterruptedException {
-            DrawSpace();
-            for(int i=0;i<snakelength;i++){
-                System.out.print("*");
-            }
-            Thread.sleep(500);
+            DrawSnake();
+            System.out.print("\033[H\033[2J");
         }
     }
-
-}
-class Game implements Runnable {
-}
-class Keyboard implements Runnable{
-    public void run()
-    {
-        try {
-            final JFrame frame = new JFrame();
-            synchronized (frame) {
-                frame.setUndecorated(true);
-                frame.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
-                frame.addKeyListener(new KeyListener() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        synchronized (frame) {
-                            frame.setVisible(false);
-                            frame.dispose();
-                            frame.notify();
-                        }
-                    }
-                    @Override
-                    public void keyReleased(KeyEvent e) {
-                        Game.snakedirections = Game.directions.down;
-                    }
-                    @Override
-                    public void keyTyped(KeyEvent e) {
-                    }
-                });
-                frame.setVisible(true);
-                try {
-                    frame.wait();
-                } catch (InterruptedException e1) {
-                }
-            }
+    public static void DrawSpace() {
+        for (int i = 0; i < snakey; i++) {
+            System.out.println();
         }
-        catch (Exception e) {
-            // Throwing an exception
-            System.out.println("Exception is caught");
+        for (int i = 0; i < snakex; i++) {
+            System.out.print(" ");
         }
     }
+    public static void DrawSnake() throws InterruptedException {
+        DrawSpace();
+        for (int i = 0; i < snakelength; i++) {
+            System.out.print("*");
+        }
+        Thread.sleep(500);
+        snakelength++;
+    }
+
 }
