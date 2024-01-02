67ba9769eccb144e87e697820875ca6b3a435b88
polnera <adampolner@gmail.com>
2024-01-02 10:53:58 -0800

    added bogosort and started double selection sort

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,8 +1,19 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class SortingAlgorithms {
+    static int iterations =0;
+    static Random rng = new Random();
     public static void main(String[] args) {//bubble, quick, double selection sort
-
+        for(;;) {
+            int[] list = new int[]{5, 3, 2, 1, 4, 7, 6, 0, 9, 8};
+            BogoSort(list);
+            System.out.println("tries: " + iterations);
+            if(iterations ==0){
+                break;
+            }
+            iterations=0;
+        }
     }
     public static void MergeSort(int[] list){//O(N Log N) performance
         if(1<list.length){
@@ -48,6 +59,28 @@ public class SortingAlgorithms {
             Swap(list,i,largest);
         }
     }
+    /*public static void DoubleSelectionSort(int[] list){
+        for(int k=list.length;k<= list.length/2;k-=2){//length of list reduces the length by 2 (largest and smallest each iteration)
+            int smallest=i;
+            for(int j=i+1;j<list.length;j++){
+                if(list[j]<list[smallest]){
+                    smallest=j;
+                }
+            }
+            Swap(list,i,smallest);
+        }
+    }*/
+    public static void BogoSort(int[] list){//O(2^N)
+        while(!isSorted(list)){
+            Shuffle(list);
+            iterations++;
+        }
+    }
+    public static void Shuffle(int[] list){
+        for(int i=0;i< list.length;i++){
+            Swap(list,list[i+rng.nextInt(list.length-i)],i);
+        }
+    }
     public static void Swap(int[] list,int i, int j){
         int temp = list[i];
         list[i]=list[j];
@@ -66,5 +99,14 @@ public class SortingAlgorithms {
                 i2++;
             }
         }
+    }
+    public static boolean isSorted(int[] list){
+        for(int i=1;i< list.length;i++){
+            int num=list[i-1];
+            if(list[i] <num){
+                return false;
+            }
+        }
+        return true;
     }
 }
