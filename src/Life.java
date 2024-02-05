88449a749a901f52a0f53338e64a03be1267c4f1
polnera <adampolner@gmail.com>
2024-02-05 09:39:46 -0800

    Started game logic

--- src/Life.java
+++ src/Life.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+
 /*
 rules:
 Any live cell with fewer than two live neighbors dies, as if by underpopulation.
@@ -19,11 +22,20 @@ public class Life extends JPanel implements ActionListener{
     }
     int tilesize=10;
     private static class Cell{
+        Tile position;
+        int neighbors;
+        Cell(Tile position){
+            this.position=position;
+        }
 
+        public void setNeighbors(int neighbors) {
+            this.neighbors = neighbors;
+        }
     }
+    Random rng = new Random();
     int BoardWidth;
     int BoardHeight;
-    int[][] pixels;
+    Cell[] cells;
     Timer frames;
     public Life(int boardwidth,int boardheight){
         BoardHeight=boardheight;
@@ -34,6 +46,27 @@ public class Life extends JPanel implements ActionListener{
         frames = new Timer(10,this);
         frames.start();
     }
+    public void GenerateCells(){
+        ArrayList<Cell> cellArrayList = new ArrayList<>();
+        for(int i=0;i<BoardWidth/(2*tilesize);i++){//half the board width as a tile size as not all the tiles need cells
+            Cell cell = new Cell(new Tile(rng.nextInt(BoardWidth/tilesize),rng.nextInt(BoardHeight/tilesize)));//creates tile at random pos
+            boolean samepos=false;
+            for(int j=0;j<cellArrayList.size();j++){//if there is already one in that position then don't add the cell
+                if((cellArrayList.get(i).position.x==cell.position.x)&&(cellArrayList.get(i).position.y==cell.position.y)){
+                    samepos=true;
+                    break;
+                }
+            }
+            if(!samepos){
+                cellArrayList.add(cell);
+            }
+        }
+        cells= new Cell[cellArrayList.size()];
+        //as it adds the cell array list that has been populated to the cells array it inserts it in order of x values,
+        //so it only needs to check the y values of 3 rows of cells in order,
+        for(int i=0;i<cells.length;i++){
+        }
+    }
 
     @Override
     public void paint(Graphics g) {
@@ -43,9 +76,21 @@ public class Life extends JPanel implements ActionListener{
     public void draw(Graphics g){
         g.setColor(new Color(255,255,255));
     }
+    public void getNeighbors(){
+
+    }
+    public void updateConditions(){
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) { //called every x milliseconds by gameLoop timer
-       repaint();
+        getNeighbors();
+        //get neighbors
+        updateConditions();
+        //update the board
+        repaint();
+        //repaint
+
     }
 }
