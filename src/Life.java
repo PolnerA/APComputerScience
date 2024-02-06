11362e734b6fee9d8f561079ec2e39d4f471a127
polnera <adampolner@gmail.com>
2024-02-06 10:54:21 -0800

    Finished life (horrible runtime)

--- src/Life.java
+++ src/Life.java
@@ -1,16 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.ArrayList;
 import java.util.Random;
 
-/*
-rules:
-Any live cell with fewer than two live neighbors dies, as if by underpopulation.
-Any live cell with two or three live neighbors lives on to the next generation.
-Any live cell with more than three live neighbors dies, as if by overpopulation.
-Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
- */
+
 public class Life extends JPanel implements ActionListener{
     private static class Tile{
         int x;
@@ -23,9 +16,11 @@ public class Life extends JPanel implements ActionListener{
     int tilesize=10;
     private static class Cell{
         Tile position;
+        Boolean alive;
         int neighbors;
         Cell(Tile position){
             this.position=position;
+            alive=true;
         }
 
         public void setNeighbors(int neighbors) {
@@ -43,28 +38,21 @@ public class Life extends JPanel implements ActionListener{
         setPreferredSize(new Dimension(BoardWidth,BoardHeight));
         setBackground(Color.black);
         setFocusable(true);
-        frames = new Timer(10,this);
+        GenerateCells();
+        frames = new Timer(5,this);
         frames.start();
     }
     public void GenerateCells(){
-        ArrayList<Cell> cellArrayList = new ArrayList<>();
-        for(int i=0;i<BoardWidth/(2*tilesize);i++){//half the board width as a tile size as not all the tiles need cells
-            Cell cell = new Cell(new Tile(rng.nextInt(BoardWidth/tilesize),rng.nextInt(BoardHeight/tilesize)));//creates tile at random pos
-            boolean samepos=false;
-            for(int j=0;j<cellArrayList.size();j++){//if there is already one in that position then don't add the cell
-                if((cellArrayList.get(i).position.x==cell.position.x)&&(cellArrayList.get(i).position.y==cell.position.y)){
-                    samepos=true;
-                    break;
+        cells= new Cell[(BoardWidth/tilesize)*(BoardHeight/tilesize)];
+        int index=0;
+        for(int i=0;i<(BoardWidth/tilesize);i++){
+            for(int j=0;j<(BoardHeight/tilesize);j++){
+                cells[index]=new Cell(new Tile(i,j));
+                if(rng.nextInt(2)==1){
+                    cells[index].alive=false;
                 }
+                index++;
             }
-            if(!samepos){
-                cellArrayList.add(cell);
-            }
-        }
-        cells= new Cell[cellArrayList.size()];
-        //as it adds the cell array list that has been populated to the cells array it inserts it in order of x values,
-        //so it only needs to check the y values of 3 rows of cells in order,
-        for(int i=0;i<cells.length;i++){
         }
     }
 
@@ -75,17 +63,74 @@ public class Life extends JPanel implements ActionListener{
     }
     public void draw(Graphics g){
         g.setColor(new Color(255,255,255));
+        for (int i = 0; i < cells.length; i++) {
+            Cell cell= cells[i];
+            if(cell.alive) {
+                g.fillRect(cell.position.x * tilesize, cell.position.y * tilesize, tilesize, tilesize);
+            }
+        }
     }
-    public void getNeighbors(){
-
+    public void CalculateNeighbors(){//N^2 Cell one and two don't need to be looked entirely through
+        for (int i = 0; i < cells.length; i++) {
+            Cell cell1 = cells[i];
+            cell1.setNeighbors(0);
+            int x = cell1.position.x;
+            int y = cell1.position.y;
+            for (int j = 0; j < cells.length; j++) {
+                Cell cell2 = cells[j];
+                if(cell2.alive) {
+                    int x2 = cell2.position.x;
+                    int y2 = cell2.position.y;
+                    if (x == x2 + 1) {//cell two is to the left
+                        if (y == y2 + 1) {//cell two is to the bottom left
+                            cell1.neighbors++;
+                        } else if (y == y2) {//cell two is to the left
+                            cell1.neighbors++;
+                        } else if (y == y2 - 1) {//cell two is to the top left
+                            cell1.neighbors++;
+                        }
+                    } else if (x == x2) {//cell two is in line with cell one
+                        if (y == y2 + 1) {//cell two is on the bottom
+                            cell1.neighbors++;
+                        } else if (y == y2 - 1) {//cell two is above
+                            cell1.neighbors++;
+                        }
+                    } else if (x == x2 - 1) {//cell two is on the right
+                        if (y == y2 + 1) {//cell two is to the bottom right
+                            cell1.neighbors++;
+                        } else if (y == y2) {//cell two is to the right
+                            cell1.neighbors++;
+                        } else if (y == y2 - 1) {//cell two is to the top right
+                            cell1.neighbors++;
+                        }
+                    }
+                }
+            }
+        }
     }
     public void updateConditions(){
-
+        /*
+        rules:
+        Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+        Any live cell with two or three live neighbors lives on to the next generation.
+        Any live cell with more than three live neighbors dies, as if by overpopulation.
+        Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+         */
+        for (int i=0;i< cells.length;i++){
+            Cell cell =cells[i];
+            if(cell.neighbors<2){
+                cell.alive=false;
+            }else if(3<cell.neighbors){
+                cell.alive=false;
+            } else if (cell.neighbors==3) {
+                cell.alive=true;
+            }
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) { //called every x milliseconds by gameLoop timer
-        getNeighbors();
+        CalculateNeighbors();
         //get neighbors
         updateConditions();
         //update the board
