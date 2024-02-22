c66ac799f1392c53b606c9b0cc682605efa78844
polnera <adampolner@gmail.com>
2024-02-22 08:57:06 -0800

    Set up start of project

--- src/Recursion/GS10_01.java
+++ src/Recursion/GS10_01.java
@@ -1,4 +1,10 @@
 package Recursion;
 
 public class GS10_01 {
+    public static void main(String[] args) {
+        fibonacci(30);
+    }
+    public static void fibonacci(int n){
+
+    }
 }
--- src/Recursion/GS10_02.java
+++ src/Recursion/GS10_02.java
@@ -1,4 +1,10 @@
 package Recursion;
 
 public class GS10_02 {
+    public static void main(String[] args) {
+
+    }
+    public static void writeSquares(int n){
+
+    }
 }
