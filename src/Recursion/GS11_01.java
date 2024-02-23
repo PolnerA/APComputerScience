3d64ff3e6df445eab60474c707ef8833475953b4
Adam Polner <polnera@rsd407.org>
2024-02-22 20:05:45 -0800

    Worked on Starstring

--- src/Recursion/GS11_01.java
+++ src/Recursion/GS11_01.java
@@ -3,7 +3,7 @@ package Recursion;
 public class GS11_01 {
     static int m=0;
     public static void main(String[] args) {
-        starString(3);
+        starString(2);
         System.out.print("\n"+m);
     }
     public static void starString(int n){
@@ -12,7 +12,10 @@ public class GS11_01 {
         if(n<=0){
             return;
         }
-        starString((--n));
+        n--;
+        starString(n);
+        if(n<=0){return;}
+        starString(n);
     }
     public static void starStringLoop(int n){
         for(int i=0;i<Math.pow(2,n);i++){
