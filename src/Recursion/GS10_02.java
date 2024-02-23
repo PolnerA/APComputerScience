e2526659c5a7171fb53d543b3b28863085a738ef
polnera <adampolner@gmail.com>
2024-02-23 09:24:43 -0800

    Finished GS10 - 2

--- src/Recursion/GS10_02.java
+++ src/Recursion/GS10_02.java
@@ -2,9 +2,16 @@ package Recursion;
 
 public class GS10_02 {
     public static void main(String[] args) {
-
+        writeSquares(8);//done
     }
     public static void writeSquares(int n){
-
+        if(n%2==1){System.out.print(n*n+",");}
+        n--;
+        if(n<=0){return;}
+        writeSquares(n);
+        n++;
+        if(n%2==0){
+            System.out.print(n*n+" ");
+        }
     }
 }
