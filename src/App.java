6a3506f2d07378595abdf96b23516cbdded0f85b
polnera <adampolner@gmail.com>
2024-02-29 09:00:11 -0800

    Created graphing program

--- src/App.java
+++ src/App.java
@@ -5,11 +5,11 @@ public class App extends Frame{//an extension of Frame (a window)
 
     public static void main(String[] args) throws InterruptedException {
         //sets the width and height
-        int boardWidth = 2000;
-        int boardHeight = 2000;
+        int boardWidth = 1000;
+        int boardHeight = 1000;
 
         //name of the window is set to Life, makes it visible and sets the size
-        JFrame frame = new JFrame("Life");
+        JFrame frame = new JFrame("Graph");
         frame.setSize(boardWidth, boardHeight);
         //sets the location to the top left
         frame.setLocation(0,0);
@@ -17,10 +17,10 @@ public class App extends Frame{//an extension of Frame (a window)
         //sets the window to exit on close
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Creates an instance of Life through the Life class, and adds it to the window giving focus to it
-        Life life = new Life(boardWidth, boardHeight);
-        frame.add(life);
+        Graphing graph = new Graphing(boardWidth, boardHeight);
+        frame.add(graph);
         frame.pack();
-        life.requestFocus();
+        graph.requestFocus();
         frame.setVisible(true);
     }
 }
