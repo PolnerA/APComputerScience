84cd1df7b4646d8bf2f5fb035b3154cedf5551d1
polnera <adampolner@gmail.com>
2024-03-14 08:57:53 -0700

    Started rpn equation input

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,81 @@
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-         Graphing.Function a = new Graphing.Function("5x + 10");
+        Scanner sc = new Scanner(System.in);
+        String Equation = sc.nextLine();
+        Stack<Integer> stack = new Stack<>();
+        boolean PrevIsInt = false;
+        int PrevInt=0;
+        int Int=0;
+        boolean IsInt = false;
+        //have an Integer array with ranges for the integers
+        /*55 52 +
+        if (Equation.charAt(i)=='-') {//checks if negative is an operator (minus) or negative number
+                if(i==Equation.length()-1){
+                    number = false;
+                }else if(!isNumber(Equation.charAt(i+1))){
+                    number=false;
+                }else{
+                    number=true;
+                }
 
+            }
+         */
+        int j=0; //level of digit
+        boolean intisDone=false;
+        boolean intisStarted=false;
+        for (int i = 0; i < Equation.length(); i++) {
+            char a = Equation.charAt(i);
+            if(isNumber(a)){
+                IsInt=true;
+                intisStarted=true;
+                Int = Integer.parseInt(""+a);
+            }else{
+                if(intisStarted){
+                    stack.push(PrevInt);
+                }
+                IsInt=false;
+                Int=0;
+                PrevInt=0;
+                j=0;
+
+            }
+            if(PrevIsInt&&IsInt){
+                PrevInt*=Math.pow(10,j);
+            }
+            PrevInt+=Int;
+            if(IsInt){
+                PrevIsInt=true;
+                j++;
+            }
+        }
+    }
+    public static boolean isNumber(char a){
+        try{
+            String string =""+a;
+            Integer.parseInt(string);
+        }catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+    public static boolean isOp(String string,int index){
+        char character = string.charAt(index);
+        if(character=='+'){
+            return true;
+        }if(character=='*'){
+            return true;
+        }if(character=='/'){
+            return true;
+        }if(character=='-'){
+            if(index==string.length()-1) {
+                return true;
+            }if(!isNumber(string.charAt(index+1))){
+                return true;
+            }
+        }
+        return false;
     }
 }
