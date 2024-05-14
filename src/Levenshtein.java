a6444761527e4498f4dc3bea83aad55e813cfc82
polnera <adampolner@gmail.com>
2024-05-14 10:54:58 -0700

    Did levenshtein in for loop

--- src/Levenshtein.java
+++ src/Levenshtein.java
@@ -67,33 +67,65 @@ public class Levenshtein {
             String word = sc.nextLine();
             a.add(word);
         }
+        //pseudocode: check if the two words have neighbors in common, if no then check if each of their neighbors have neighbors in common
+        //repeat with next level of neighbors
         sc.close();
         Scanner sc2 = new Scanner(System.in);
         System.out.println("First word:");
         String word1 = sc2.nextLine().toLowerCase();
         System.out.println("Second word");
         String word2 = sc2.nextLine().toLowerCase();
-        GetPath(word1,word2);
-    }
-    public static void GetPath(String word1,String word2) throws FileNotFoundException {
-        String [] neighbors = getNeighbors(word1);
-        if(isOneAway(word1,word2)){
-            System.out.println("done");
-        }else{
-            for(String neighbor:neighbors){
-                String[] ns =getNeighbors(neighbor);
-                if(isOneAway(neighbor,word2)){
-                    System.out.println("done");
-                }else{
-                    for(String neighbor2:ns){
-                        GetPath(neighbor2,word2);
+        getPath(word1,word2,false);
+        HashSet<String> b = getNeighborsSet(word1);
+        if(!b.contains(word2)){
+            for(String n:b){
+                HashSet<String> c = getNeighborsSet(n);
+                if(!c.contains(word2)){
+                    for(String neighbors:c){
+                        HashSet<String> d = getNeighborsSet(neighbors);
+                        if(!d.contains(word2)){
+                            for(String ne:d ) {
+                                HashSet<String> e = getNeighborsSet(ne);
+                                if (!e.contains(word2)){
+
+                                }else{
+                                    System.out.println(word1+"->"+n+"->"+neighbors+"->"+ne+"->"+word2);
+                                }
+                            }
+                        }else {
+                            System.out.println(word1+"->"+n+"->"+neighbors+"->"+word2);
+                        }
                     }
+                }else {
+                    System.out.println(word1+"->"+n+"->"+word2);
+
+                }
+            }
+        }else {
+            System.out.println(word1+"->"+word2);
+        }
+        System.out.println("fin");
+    }
+    public static boolean getPath(String word1, String word2, boolean goBack) throws FileNotFoundException{
+        HashSet<String> neighbors = getNeighborsSet(word1);
+        String[] neighborsArr = getNeighborsArr(word1);
+        if(neighbors.contains(word2)){
+            System.out.println("done");
+            return true;
+        }else if(!goBack){
+            for(String neighbor:neighbors){
+                if(getPath(neighbor,word2,true)){
+                    return true;
+                }else{
+
                 }
             }
         }
+        return false;
     }
-    public static String[] getNeighbors(String word) throws FileNotFoundException{
+    public static String[] getNeighborsArr(String word) throws FileNotFoundException{
         Scanner sc = new Scanner(new File("dictionaryWithNeighbors"));
+        HashSet<String> Neighbors = new HashSet<>();
         while(sc.hasNext()){
             String line =sc.nextLine();
             String [] results =line.split("-");
@@ -103,14 +135,19 @@ public class Levenshtein {
         }
         return null;
     }
-    public static boolean isOneAway(String word, String neighbor) throws FileNotFoundException {
+    public static HashSet<String> getNeighborsSet(String word) throws FileNotFoundException{
         Scanner sc = new Scanner(new File("dictionaryWithNeighbors"));
-        String [] results = getNeighbors(word);
-        for(int i=0;i<results.length;i++){
-            if(results[i].equals(neighbor)){
-                return true;
+        HashSet<String> Neighbors = new HashSet<>();
+        while(sc.hasNext()){
+            String line =sc.nextLine();
+            String [] results =line.split("-");
+            if(results[0].equals(word)){
+                for(int i=0;i< results.length;i++){
+                    Neighbors.add(results[i]);
+                }
+                return Neighbors;
             }
         }
-        return false;
+        return null;
     }
 }
