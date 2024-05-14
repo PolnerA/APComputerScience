adf1f068d8c18b734ac2a3cd4479f9b6d5e077d1
polnera <adampolner@gmail.com>
2024-05-14 10:02:02 -0700

    Fixed typo in recursion goalsheet

--- src/Recursion/GS10_01.java
+++ src/Recursion/GS10_01.java
@@ -1,6 +1,6 @@
 /*
 Name: Adam Polner
-Problem:Write a recursive method that takes an integer argument n and gives the nth number in hte fibonacci series.
+Problem:Write a recursive method that takes an integer argument n and gives the nth number in the fibonacci series.
 Pseudocode: Recursive method starts at given value n and calculates the previous 2 fib numbers adding them up, until it reaches to the first 2 digits in which case it returns 1
 Notes: better way of getting the fib number in GS11-05
 Maintenance log:
