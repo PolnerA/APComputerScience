a24823c3599b761e3cfb177c8be6338d6ad40bf3
polnera <adampolner@gmail.com>
2024-05-28 10:53:45 -0700

    Made the algorithm for solves more efficient

--- src/Levenshtein.java
+++ src/Levenshtein.java
@@ -2,13 +2,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Time;
 import java.util.*;
 
 public class Levenshtein {
-    static HashSet<String> a = new HashSet<>();
+    static HashSet<String> Dictionary = new HashSet<>();
     static int paths=0;//reasonable estimate for shortest path
-    static final boolean getPaths = false;
+    static final boolean getPaths = true;
+    ArrayList<String> from = new ArrayList<>();
+    int index;
+
     public static void preCompute() throws IOException {
         Scanner sc2 = new Scanner(new File("dictionarySortedLength.txt"));
         File neighbor = new File("dictionaryWithNeighbors");
@@ -18,7 +20,7 @@ public class Levenshtein {
         while(sc2.hasNext()){ //_0_1_2_3_4_
             String word = sc2.nextLine();
             count++;
-            System.out.println((double) count/(double) a.size());
+            System.out.println((double) count/(double) Dictionary.size());
             char[] letters=word.toCharArray();
             ArrayList<Character> letter = new ArrayList<>();
             ArrayList<Character> letter2 = new ArrayList<>();
@@ -36,7 +38,7 @@ public class Levenshtein {
                         for (int k = 0; k < letter2.size(); k++) {
                             newWord = newWord + letter2.get(k);
                         }
-                        if (a.contains(newWord)) {
+                        if (Dictionary.contains(newWord)) {
                             if (!word.equals(newWord)) {
                                 writer.write("-" + newWord);
                             }
@@ -49,7 +51,7 @@ public class Levenshtein {
                     for(int k=0;k<letter2.size();k++){
                         newWord2= newWord2 + letter2.get(k);
                     }
-                    if(a.contains(newWord2)){
+                    if(Dictionary.contains(newWord2)){
                         writer.write("-"+newWord2);
                     }
                     letter2=new ArrayList<>();
@@ -62,118 +64,24 @@ public class Levenshtein {
         sc2.close();
 
     }
-    public static class queue implements java.util.Queue {
-        public queue() {
-
-        }
-        @Override
-        public int size() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean contains(Object o) {
-            return false;
-        }
-
-        @Override
-        public Iterator iterator() {
-            return null;
-        }
-
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @Override
-        public Object[] toArray(Object[] a) {
-            return new Object[0];
-        }
-
-        @Override
-        public boolean add(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(Collection c) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @Override
-        public boolean retainAll(Collection c) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(Collection c) {
-            return false;
-        }
-
-        @Override
-        public boolean containsAll(Collection c) {
-            return false;
-        }
-
-        @Override
-        public boolean offer(Object o) {
-            return false;
-        }
-
-        @Override
-        public Object remove() {
-            return null;
-        }
-
-        @Override
-        public Object poll() {
-            return null;
-        }
-
-        @Override
-        public Object element() {
-            return null;
-        }
-
-        @Override
-        public Object peek() {
-            return null;
-        }
-    }
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(new File("dictionarySortedLength.txt"));
         while(sc.hasNext()){
           String word = sc.nextLine();
-          a.add(word);
+          Dictionary.add(word);
         }
         sc.close();
         //tests: cat to dog, dog to cat, puppy to dog, dog to smart, dog to quack, monkey to business
         //test 1       24815.761561 ms predicted runtime (about 25 sec)
-        //              Current time: 6085 ms run (10000) times : 3ms
-        //              100 times: 13 ms
+        //              Current time: 115 ms run (10000) times : 3ms
+        //              100 times: 12 ms
         //String word1="cat";
         //String word2="dog";
         //test 2       15703.301474 ms predicted runtime (about 16 sec)
-        //              Current time: 1475 ms run (10000) times : 16 ms
+        //              Current time: 284 ms run (10000) times : 16 ms
         //              100 times: 31 ms
-        String word1="dog";
-        String word2="cat";
+        //String word1="dog";
+        //String word2="cat";
         //test 3      185795.471987 ms predicted runtime (about 3 min 6 sec)
         //                  current time : 14996 ms
         //            100 times:
@@ -182,8 +90,8 @@ public class Levenshtein {
         //test 4      189735.666912 ms predicted runtime (about 3 min 10 sec)
         //              current time : 1813 ms
         //            100 times: 574 ms
-        //String word1="dog";
-        //String word2="smart";
+        String word1="dog";
+        String word2="smart";
         //test 5     1732498.366852 ms predicted runtime (about 29 min)
         //          current time : 4722 ms
         //            100 times: 2461 ms
@@ -210,38 +118,34 @@ public class Levenshtein {
     }
     public static void solve(String word1, String word2) throws FileNotFoundException {
         HashSet<String> usedWords = new HashSet<>();
-        HashSet<String> neighbors = getNeighborsSet(word1);//gets the neighbors of the first word
         Queue<String> queue = new LinkedList<>();//queue should replace the stack
-        for(String word:neighbors) {
-            queue.add(word);//populates
-        }
+        queue.add(word1);//
         while (!queue.isEmpty()) {//while there are neighbors
             String word = queue.remove();//current neighbor is assumed
-            if(usedWords.contains(word)){
-                continue;
-            }
             HashSet<String> currentNeighbors = getNeighborsSet(word);//the current neighbors in the assumed word
             usedWords.add(word);
-            if (!currentNeighbors.contains(word2)) {//if that neighbor isn't
-                    for(String neighbor:currentNeighbors){
-                        queue.add(neighbor);
-                    }
+            currentNeighbors.removeAll(usedWords);
+            if (!currentNeighbors.contains(word2)) {
+                queue.addAll(currentNeighbors);
             } else {
                 break;
             }
         }
     }
     public static void printsolves(String word1, String word2) throws FileNotFoundException {
-        HashSet<String> neighbors = getNeighborsSet(word1);//gets the neighbors of the first word
         Queue<String> queue = new LinkedList<String>();//queue should replace the stack
-        for(String word:neighbors) {
-            queue.add(word);//populates
-        }
-        int levdist=0;//queues read from head and adds to tail, stack would read from head and add to head
+        boolean found=false;
+        queue.add(word1);//populates    //w ""
+        queue.add("");                  // 1 2 3 4 5 ""
+                                        //11 12 13 14 15"" 21 22 23 24 ""
         while (!queue.isEmpty()) {//while there are neighbors
             String wordpath = queue.remove();//current neighbor is assumed
-            String word;
-            word = getValue(wordpath);
+            if(wordpath.equals("")){
+                if(found){break;}
+                queue.add("");
+                continue;
+            }
+            String word = getValue(wordpath);
             HashSet<String> currentNeighbors = getNeighborsSet(word);//the current neighbors in the assumed word
             if (!currentNeighbors.contains(word2)) {//if that neighbor isn't
                 for(String neighbor:currentNeighbors){
@@ -249,14 +153,10 @@ public class Levenshtein {
                 }
             } else {
                 wordpath = wordpath+"->"+word2;
-                if(levdist==0){
-                    levdist=getLevDist(wordpath);
-                }
-                if(levdist<getLevDist(wordpath)){
-                    break;
-                }
                 paths++;
                 System.out.println(word1+"->"+wordpath+" #"+paths);
+                found=true;
+
             }
         }
     }
@@ -298,7 +198,7 @@ public class Levenshtein {
                     for (int k = 0; k < letter2.size(); k++) {
                         newWord = newWord + letter2.get(k);
                     }
-                    if (a.contains(newWord)) {
+                    if (Dictionary.contains(newWord)) {
                         if (!word.equals(newWord)) {
                             neighbors.add(newWord);
                         }
@@ -311,7 +211,7 @@ public class Levenshtein {
                 for(int k=0;k<letter2.size();k++){
                     newWord2= newWord2 + letter2.get(k);
                 }
-                if(a.contains(newWord2)){
+                if(Dictionary.contains(newWord2)){
                     neighbors.add(newWord2);
                 }
                 letter2=new ArrayList<>();
