7f30563aae01ec333202f6e5d670d9d3aa61a655
polnera <adampolner@gmail.com>
2024-06-04 10:53:28 -0700

    Started trimming down the paths

--- src/Levenshtein.java
+++ src/Levenshtein.java
@@ -6,23 +6,13 @@ import java.util.*;
 
 public class Levenshtein {
     static HashSet<String> Dictionary = new HashSet<>();
-    public static class Neighbors{
-        public HashSet<Neighbors> neighbors;
-        public int[] OutsideNeighbors;
-        public String word;
-        public Neighbors(String word){
-            this.word=word;
-        }
-    }
-    HashMap<String,Integer> a ;//same as dictionary arr
     //type of datatype to use first it has to store a value (the word), and it needs to point to other values (neighbors)
     //Hashmap of a string would store the values with an integer array to store the different neighbors
-    static String[] DictionaryArr;//these two arrays are what is looked at try to convert to a single hash thing
     //
-    static ArrayList<String>[] neighborsArray;
-    static int paths=0;//reasonable estimate for shortest path
-    static final boolean getPaths = false;
-    ArrayList<String> from = new ArrayList<>();
+    static HashMap<String,HashSet<String>> neighbors= new HashMap<>();
+    static ArrayList<ArrayList<String>> paths = new ArrayList<>();
+    static int pathNumber=0;//reasonable estimate for shortest path
+    static final boolean getPaths = true;
 
     public static void preCompute() throws IOException {
         Scanner sc2 = new Scanner(new File("dictionarySortedLength.txt"));
@@ -81,27 +71,21 @@ public class Levenshtein {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(new File("dictionarySortedLength.txt"));
         int index=0;
-        DictionaryArr=new String[370099];
         while(sc.hasNext()){
           String word = sc.nextLine();
           Dictionary.add(word);
-          DictionaryArr[index]=word;
           index++;
         }
         sc.close();
-        neighborsArray = new ArrayList[Dictionary.size()];
         Scanner sc2 = new Scanner(new File("dictionaryWithNeighbors"));
-        int count=0;
         while (sc2.hasNext()){//goes through with the neighbors
-            String word = sc2.nextLine();
-            String[] neighborsarr =word.split("-");
-            Neighbors nOfWord = new Neighbors(neighborsarr[0]);
-            ArrayList<String> neighbors = new ArrayList<>();
-            for(int i=1;i<neighborsarr.length;i++){
-                neighbors.add(neighborsarr[i]);
+            String Line =sc2.nextLine();
+            String[] neighborsLine = Line.split("-");
+            HashSet<String> neighborsSet = new HashSet<>();
+            for(int i=1;i<neighborsLine.length;i++){
+                neighborsSet.add(neighborsLine[i]);
             }
-            neighborsArray[count]=neighbors;
-            count++;
+            neighbors.put(neighborsLine[0],neighborsSet);
         }
         System.out.println("");
         //for(int i=0;i<neighborsArray.length;i++){
@@ -117,39 +101,39 @@ public class Levenshtein {
         //tests: cat to dog, dog to cat, puppy to dog, dog to smart, dog to quack, monkey to business
         //shortest paths: 6,     6     ,      38     ,      51     ,      107    ,       1
         //test 1       24815.761561 ms predicted runtime (about 25 sec)
-        //              Current time: 67 ms run (10000) times : 3ms
-        //              100 times: 12 ms | all paths work
+        //              Current time: 2 ms run (10000) times : 3ms
+        //              100 times: 0 ms | all paths work
         //String word1="cat";
         //String word2="dog";
         //test 2       15703.301474 ms predicted runtime (about 16 sec)
-        //              Current time: 284 ms run (10000) times : 4 ms
-        //              100 times: 31 ms | all paths work
+        //              Current time: 5 ms run (10000) times : 4 ms
+        //              100 times: 1 ms | all paths work
         //String word1="dog";
         //String word2="cat";
         //test 3      185795.471987 ms predicted runtime (about 3 min 6 sec)
-        //                  current time : 14996 ms
-        //            100 times:   | all paths
-        //String word1="puppy";
-        //String word2="dog";
+        //                  current time : 130 ms
+        //            100 times: 137 ms  | all paths throws out of memory, all past this do too
+        String word1="puppy";
+        String word2="dog";
         //test 4      189735.666912 ms predicted runtime (about 3 min 10 sec)
-        //              current time : 1813 ms
-        //            100 times: 574 ms | all paths work without string1->string2 throws out of memory pre finishing
-        String word1="dog";
-        String word2="smart";
+        //              current time : 132 ms
+        //            100 times: 14 ms | all paths work without string1->string2 throws out of memory pre finishing
+        //String word1="dog";
+        //String word2="smart";
         //test 5     1732498.366852 ms predicted runtime (about 29 min)
-        //          current time : 4722 ms
-        //            100 times: 2461 ms | all paths give java.lang.OutOfMemoryError
+        //          current time : 997 ms
+        //            100 times: 42 ms | all paths give java.lang.OutOfMemoryError
         //String word1="dog";
         //String word2="quack";
         //test 6     2814028.051959 ms predicted runtime (about 47 min)
-        //          current time : 12206 ms
-        //            100 times:
+        //          current time : 1054 ms
+        //            100 times: 103 ms
         //String word1="monkey";
         //String word2="business";
         //to solve out of memory improve the maps to smaller sizes and use vm options: -Xlog:gc to print the garbage collector
         if(!getPaths){
             Long sum = Long.valueOf(0);
-            int num = 1;
+            int num = 100;
             for(int i=0;i<num;i++){
                 Long pre = System.currentTimeMillis();
                 solve(word1,word2);
@@ -162,12 +146,12 @@ public class Levenshtein {
         }
     }
     public static void solve(String word1, String word2)  {
-        HashSet<String> usedWords = new HashSet<>();
+        HashSet<String> usedWords = new HashSet<>();//not repeating a word helps keep the out of memory error away
         Queue<String> queue = new LinkedList<>();
         queue.add(word1);
         while (!queue.isEmpty()) {//while there are neighbors
             String word = queue.remove();//current neighbor is assumed
-            HashSet<String> currentNeighbors = getNeighborsSet(word);//the current neighbors in the assumed word
+            HashSet<String> currentNeighbors = neighbors.get(word);//the current neighbors in the assumed word
             usedWords.add(word);
             currentNeighbors.removeAll(usedWords);
             if (!currentNeighbors.contains(word2)) {
@@ -176,9 +160,33 @@ public class Levenshtein {
                 return;
             }
         }
+
     }
-    public static void printsolves(String word1, String word2) {//java.lang.OutOfMemoryError
+    /*
+    public static void printsolves(String word1, String word2){//implement into this with the different paths being used only in a self-contained "path"
+        Queue<String> queue = new LinkedList<>();
+        queue.add(word1);
+        while (!queue.isEmpty()) {//while there are neighbors
+            String word = queue.remove();//current neighbor is assumed
+            HashSet<String> currentNeighbors = neighbors.get(word);//the current neighbors in the assumed word
+            if (!currentNeighbors.contains(word2)) {
+                queue.addAll(currentNeighbors);
+            } else {
+                return;
+            }
+        }
+    }
+
+     */
+
+    public static void printsolves(String word1, String word2) {//java.lang.OutOfMemoryError as there are
+        //visualize 2d list as a binary tree
+        //     word1n1 word1n2 word1n3//copy for each neighbor and add them to the list
+        //keep count of the traversal i for bottom and j for top
+
+        // list word1
         Queue<String> queue = new LinkedList<String>();//queue created once and used
+        Queue<Stack<String>> paths = new LinkedList<>();
         boolean found=false;//found boolean helps with the final line
         queue.add(word1);//populates    //w ""   try to get a sentinel of empty string for each distance
         queue.add("");                  // 1 2 3 4 5 ""
@@ -190,29 +198,29 @@ public class Levenshtein {
                 queue.add("");//skips over sentinel otherwise and adds a new one to the end line marking the neighbors end
                 continue;
             }
-            String word = getValue(wordpath);
-            HashSet<String> currentNeighbors = getNeighborsSet(word);//the current neighbors in the assumed word
+            //String word = getValue(wordpath);
+            HashSet<String> currentNeighbors = neighbors.get(wordpath);//the current neighbors in the assumed word
             if (!currentNeighbors.contains(word2)) {//if that neighbor isn't
-                for(String neighbor:currentNeighbors){
-                    queue.add(wordpath+"->"+neighbor);
+                if(!found){//only add neighbors if the shortest isn't on this level
+                    for(String neighbor:currentNeighbors){
+                        //store as a path of strings so you know to eliminate paths that come after the path
+                        queue.add(neighbor);
+                    }
                 }
             } else {
                 wordpath = wordpath+"->"+word2;
-                paths++;
-                System.out.println(word1+"->"+wordpath+" #"+paths);
+                pathNumber++;
+                System.out.println(word1+"->"+wordpath+" #"+pathNumber);
                 found=true;
 
             }
         }
     }
-    public static int getLevDist(String path){
-        String[] a=path.split("->");
-        return a.length;
-    }
     public static String getValue(String path){
         //from the word1 +"->" +neighbors get the last arrow thing
         return path.substring(path.lastIndexOf(">")+1);
     }
+    /*
     public static HashSet<String> getNeighborsSet(String word) {
         ArrayList<Character> letter = new ArrayList<>();
         ArrayList<Character> letter2 = new ArrayList<>();
@@ -253,4 +261,5 @@ public class Levenshtein {
         }
         return neighbors;
     }
+     */
 }
